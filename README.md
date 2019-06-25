2c3476428566002dda7e540bb8fadb7e17bcd732
XATTpy <zoomerrr@mail.ru>
2019-06-25 19:30:33 +0300

    add functions, unpdate readme and add blacklist.txt

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+This script evaluates the password you entered based on generally accepted criteria.
+
+# Quickstart
+
+```bash
+$ python3 password_strength.py
+Input your password:
+Input your name: XATTpy
+Your password rating: 8
+```
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,45 @@
+from getpass import getpass
+import re
 
 
-def get_password_strength(password):
-    pass
+def load_blacklist(file):
+    with open(file, 'r') as opened_file:
+        return opened_file.read().split()
 
 
-if __name__ == '__main__':
-    pass
+def get_password_strength(password, username, blacklist):
+    password_rating = 10
+
+    if password in blacklist:
+        password_rating = 1
+        return password_rating
+
+    if not re.findall(r"[a-z]", password):
+        password_rating -= 2
+    if not re.findall(r"[A-Z]", password):
+        password_rating -= 2
+    if not re.findall(r"[0-9]", password):
+        password_rating -= 2
+    if not re.findall(r"\W", password):
+        password_rating -= 2
+    if username.lower() in password.lower():
+        password_rating -= 2
+    return password_rating
+
+
+def show_password_rating(password_rating):
+    print("Your password rating: {}".format(password_rating))
+
+    
+if __name__ == "__main__":
+
+    password = getpass(prompt="Input your password: ")
+    if not password:
+        quit("Password is empty.")
+    if len(password) < 6:
+        quit("Password must include 6 or more characters")
+
+    username = input("Input your name: ")
+    blacklist = load_blacklist("blacklist.txt")
+    password_rating = get_password_strength(password, username, blacklist)
+    show_password_rating(password_rating)
